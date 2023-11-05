30441e4a87cb01a8cbb3010478a8acfc44c4f197
Akash374 <akash.hossain@dataphilics.in>
2023-11-05 22:43:23 +0600

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import "./App.css";
-// import Example from "./components/Example";
 import Gallery from "./components/Gallery";
 
 function App() {
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,8 @@
 import { useRef, useState } from "react";
 import { BiSolidSelectMultiple } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
+import { IoMdImages } from "react-icons/io";
 import { imgPath } from "../assets/data/imagePath";
-import "./Example.css";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const record = [
   false,
@@ -20,79 +19,46 @@ const record = [
 ];
 
 const Gallery = () => {
+  const dragItem = useRef();
+  const dragOverItem = useRef();
   const [list, setList] = useState(imgPath);
   const [selectedList, setSelectedList] = useState([]);
 
-  const drop = (results) => {
-    const { source, destination, type } = results;
+  const dragStart = (e, position) => {
+    dragItem.current = position;
+  };
+  const dragEnter = (e, position) => {
+    dragOverItem.current = position;
+  };
 
-    if (!destination) return;
-
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    )
-      return;
-
-    if (type === "group") {
-      const reorderedList = [...list];
-
-      const sourceIndex = source.index;
-      const destinationIndex = destination.index;
-
-      const [removedStore] = reorderedList.splice(sourceIndex, 1);
-      reorderedList.splice(destinationIndex, 0, removedStore);
-
-      return setList(reorderedList);
-    }
-    const itemSourceIndex = source.index;
-    const itemDestinationIndex = destination.index;
-
-    const sourceIndex = list.findIndex(
-      (store) => store.id === source.droppableId
-    );
-    const destinationIndex = list.findIndex(
-      (store) => store.id === destination.droppableId
-    );
-
-    const newSourceItems = [...list[sourceIndex].items];
-    const newDestinationItems =
-      source.droppableId !== destination.droppableId
-        ? [...list[destinationIndex].items]
-        : newSourceItems;
-
-    const [deletedItem] = newSourceItems.splice(itemSourceIndex, 1);
-    newDestinationItems.splice(itemDestinationIndex, 0, deletedItem);
-
-    const newList = [...list];
-
-    newList[sourceIndex] = {
-      ...list[sourceIndex],
-      items: newSourceItems,
-    };
-    newList[destinationIndex] = {
-      ...list[destinationIndex],
-      items: newDestinationItems,
-    };
-
-    setList(newList);
+  const drop = () => {
+    const copyListItems = [...list];
+    const dragItemContent = copyListItems[dragItem.current]; //take the dragged item
+    copyListItems.splice(dragItem.current, 1); //remove the dragged item from the list
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent); //added the dragged item over which it was dragged
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setList(copyListItems);
   };
 
   const handleAddToSelectedList = (e, selectedItemid) => {
-    console.log(selectedItemid, record[selectedItemid]);
+    //filtered out the selected item
     if (!record[selectedItemid]) {
       const selectedItem = list.filter(
         (listItem) => listItem.id === selectedItemid
       );
-      //console.log(selectedItem);
+      //set the selected item as the selected item
       record[selectedItemid] = true;
+      //add the selected item to the selected list
       setSelectedList([...selectedList, ...selectedItem]);
     } else {
+      //remove the selected item from the selected list
       const restItems = selectedList.filter(
         (item) => item.id !== selectedItemid
       );
-      //console.log(restItems);
+      //set the selected item as not selected
       record[selectedItemid] = false;
+      //update state with rest items
       setSelectedList(restItems);
     }
   };
@@ -102,7 +68,6 @@ const Gallery = () => {
     for (let i = 0; i < selectedList.length; i++) {
       ids.push(selectedList[i].id);
     }
-    console.log(ids);
     const filesAfterDelete = list.filter(
       (listItem) => !ids.includes(listItem.id)
     );
@@ -114,9 +79,9 @@ const Gallery = () => {
   };
 
   return (
-    <>
+    <div className="w-4/5 mx-auto h-full">
       {selectedList.length !== 0 && (
-        <div className="flex justify-around items-center m-2 p-2 font-bold border-2 border-purple-500 rounded-lg sticky top-0 bg-white z-50">
+        <div className="flex justify-around items-center m-2 p-2 font-bold border-2 rounded-lg sticky top-0 bg-white z-50">
           <div className="flex justify-center items-center gap-2">
             <BiSolidSelectMultiple />
             <p className="text-center">
@@ -141,75 +106,60 @@ const Gallery = () => {
           <p>Gallery</p>
         </div>
       )}
-      <DragDropContext onDragEnd={drop}>
-        <Droppable droppableId="ROOT" type="group">
-          {(provided) => (
+
+      <div className="grid md:grid-cols-5 shadow-lg gap-2 m-2">
+        {list &&
+          list.map((item, index) => (
             <div
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              className="flex justify-center"
+              className={`${
+                index == 0 ? "row-span-2 col-span-2 " : ""
+              }group relative shadow-lg rounded-lg border border-purple-500 cursor-pointer item`}
+              key={index}
+              draggable={!record[item.id]}
+              onDragStart={(e) => dragStart(e, index)}
+              onDragEnter={(e) => dragEnter(e, index)}
+              onDragEnd={drop}
+              onDragOver={(e) => e.preventDefault()}
             >
-              <div className="grid md:grid-cols-5 shadow-lg gap-2 m-2 w-[70%]">
-                {list &&
-                  list.map((item, index) => (
-                    <Draggable
-                      draggableId={item.id}
-                      index={index}
-                      key={item.id}
-                    >
-                      {(provided) => (
-                        <div
-                          {...provided.dragHandleProps}
-                          {...provided.draggableProps}
-                          ref={provided.innerRef}
-                          className={`${
-                            index == 0 ? "row-span-2 col-span-2 " : " "
-                          }group relative shadow-lg rounded-lg border-2 border-purple-500 cursor-pointer `}
-                          // draggable={!record[item.id]}
-                          // onDragStart={(e) => dragStart(e, index)}
-                          // onDragEnter={(e) => dragEnter(e, index)}
-                          // onDragEnd={drop}
-                          // onDragOver={(e) => e.preventDefault()}
-                        >
-                          <input
-                            type="checkbox"
-                            name=""
-                            checked={record[item.id]}
-                            id={index}
-                            className={`${
-                              !record[item.id]
-                                ? "hidden group-hover:block"
-                                : "block"
-                            } absolute top-2 left-2 transition-transform duration-500 h-6 w-6 cursor-pointer z-40`}
-                            onClick={(e) => handleAddToSelectedList(e, item.id)}
-                          />
-                          <img
-                            src={item.path}
-                            alt=""
-                            className={`w-[100%] h-[100%] group-hover:shadow transition-transform transform group-hover:scale-105 duration-500 rounded-md`}
-                          />
-                          <div
-                            className={`${
-                              record[item.id]
-                                ? "opacity-40"
-                                : "shadow-lg opacity-0 group-hover:opacity-40 group-hover:scale-105"
-                            } absolute top-0 left-0 w-full h-full  transition-opacity duration-300 flex items-center justify-center p-4 rounded-md bg-black`}
-                          ></div>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                {provided.placeholder}
-              </div>
+              <input
+                type="checkbox"
+                name=""
+                checked={record[item.id]}
+                id={index}
+                className={`${
+                  !record[item.id] ? "hidden group-hover:block" : "block"
+                } absolute top-2 left-2 transition-transform duration-500 h-6 w-6 cursor-pointer z-40`}
+                onClick={(e) => handleAddToSelectedList(e, item.id)}
+              />
+              <img
+                src={item.path}
+                alt=""
+                className={`w-full h-full group-hover:shadow transition-transform transform duration-500 rounded-md`}
+              />
+              <div
+                className={`${
+                  record[item.id]
+                    ? "opacity-40"
+                    : "shadow-lg opacity-0 group-hover:opacity-40 "
+                } absolute top-0 left-0 w-full h-full  transition-opacity duration-300 flex items-center justify-center p-4 rounded-md bg-black`}
+              ></div>
             </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+          ))}
+        <div className="relative flex justify-center items-center">
+          <input
+            type="file"
+            accept="image/*"
+            className="z-10 opacity-0 w-full h-full cursor-pointer"
+          />
+          <div className="absolute top-0 left-0 flex flex-col justify-center items-center gap-y-4 border-4 border-dotted shadow-lg w-full h-full">
+            <IoMdImages className="w-8 h-8" />
+            <p className="font-bold">Add Images</p>
+          </div>
+        </div>
+      </div>
       {list.length === 0 && <p className="text-center">Gallery is empty!!!</p>}
-    </>
+    </div>
   );
 };
 
 export default Gallery;
-
-//className={`${index == 0 ? "w-[100%] h-[100%]" : ""} p-2`}
